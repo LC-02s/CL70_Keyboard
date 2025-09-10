44cf6cd67dfa57d68a1a31291cde9053ec5e0f83
LC-02s <chanlee1007@naver.com>
2025-09-10 18:19:41 +0900

    Update Rev 1.1

--- Firmware/keymaps/via/config.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-/* Change USB Polling Rate to 1000hz and a larger keys per scan for elite gaming*/
-#define USB_POLLING_INTERVAL_MS 1
-#define QMK_KEYS_PER_SCAN 12
---
+++ PCB/Firmware/config.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define NKRO_DEFAULT_ON true
+
+/* VIA related config */
+#define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 1
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+
+/* Rotary Encoder related config */
+#define ENCODER_A_PINS { D1 }
+#define ENCODER_B_PINS { D2 }
+#define ENCODER_MAP_KEY_DELAY 10
+
+/* Layer Lock related config */
+#define LAYER_INDICATOR_PIN B7
+#define LAYER_LOCK_INDICATOR 3
